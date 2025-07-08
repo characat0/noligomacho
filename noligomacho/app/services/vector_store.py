2811d6f0132ba2da553fd0fe0d24ab7f10b042e5
characat0 <36453977+characat0@users.noreply.github.com>
2025-07-08 13:36:31 +0100

    add document splitting functionality to vector store service

--- noligomacho/app/services/vector_store.py
+++ noligomacho/app/services/vector_store.py
@@ -5,7 +5,7 @@ from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_ollama import OllamaEmbeddings
 from langchain_core.documents import Document
 from tempfile import SpooledTemporaryFile
-
+from langchain_text_splitters import CharacterTextSplitter
 
 @lru_cache(None)
 class VectorStoreService:
@@ -14,14 +14,27 @@ class VectorStoreService:
             OllamaEmbeddings(model="llama3.2:1b", temperature=0),
         )
         self.retriever = self.vector_store.as_retriever()
+        self.text_splitter = CharacterTextSplitter(
+            chunk_size=256,
+            chunk_overlap=32,
+        )
+
+    def split_documents(self, documents: list[Document]) -> list[Document]:
+        """Split a document into smaller chunks."""
+        # For simplicity, we return the document as is.
+        # In a real application, you might want to implement a more sophisticated splitting logic.
+        return self.text_splitter.split_documents(documents)
 
     def add_file(self, file: SpooledTemporaryFile | BinaryIO) -> str:
         """Add a document to the vector store."""
-        document = Document(
-            page_content=file.read().decode('utf-8'),
-            metadata={"source": file.name}
-        )
-        return self.vector_store.add_documents([document])[0]
+        documents = [
+            Document(
+                page_content=file.read().decode('utf-8'),
+                metadata={"source": file.name}
+            )
+        ]
+        documents = self.text_splitter.split_documents(documents)
+        return self.vector_store.add_documents(documents)[0]
 
     def add_files(self, files: list[SpooledTemporaryFile | BinaryIO]) -> list[str]:
         """Add multiple documents to the vector store."""
@@ -29,6 +42,7 @@ class VectorStoreService:
             Document(page_content=file.read().decode('utf-8'), metadata={"source": file.name})
             for file in files
         ]
+        documents = self.text_splitter.split_documents(documents)
         return self.vector_store.add_documents(documents)
 
     def similarity_search(self, query, k=5):
